97f702f064e8c74e0e6d4767f039514dd2caacab
vietchung06 <cn654770@gmail.com>
2026-06-19 08:27:26 +0700

    Màn hình phương thức thanh toán

---
+++ app/src/main/java/fpoly/DatnMD06Su26/trendify/activity/OrderConfirmActivity.java
@@ -0,0 +1,13 @@
+package fpoly.DatnMD06Su26.trendify.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import fpoly.DatnMD06Su26.trendify.R;
+
+public class OrderConfirmActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_confirm);
+    }
+}
