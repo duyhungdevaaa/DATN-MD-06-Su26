5cebe56977c09ee8d3cdcf8e0ca669b710bb7cc9
duyhungdevaaa <machhung1720@gmail.com>
2026-07-02 17:24:44 +0700

    tạo trang tổng quan dashboard web admin

--- app/src/main/java/fpoly/DatnMD06Su26/trendify/fragment/CategoryFragment.java
+++ app/src/main/java/fpoly/DatnMD06Su26/trendify/fragment/CategoryFragment.java
@@ -1,18 +1,46 @@
 package fpoly.DatnMD06Su26.trendify.fragment;
 
+import fpoly.DatnMD06Su26.trendify.R;
+import fpoly.DatnMD06Su26.trendify.adapter.CategoryAdapter;
+import fpoly.DatnMD06Su26.trendify.model.CategoryItem;
+import fpoly.DatnMD06Su26.trendify.helper.FirestoreHelper;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import fpoly.DatnMD06Su26.trendify.R;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CategoryFragment extends Fragment {
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_category, container, false);
+        
+        RecyclerView rvCategories = view.findViewById(R.id.rvCategories);
+        
+        CategoryAdapter adapter = new CategoryAdapter(new ArrayList<>());
+        rvCategories.setAdapter(adapter);
+
+        FirestoreHelper.loadCategories(new FirestoreHelper.CategoriesCallback() {
+            @Override
+            public void onLoaded(List<CategoryItem> categories) {
+                adapter.setItems(categories);
+            }
+
+            @Override
+            public void onFailure(String error) {
+                Toast.makeText(requireContext(), "Không thể tải danh mục: " + error, Toast.LENGTH_LONG).show();
+            }
+        });
+        
+        return view;
     }
 }
