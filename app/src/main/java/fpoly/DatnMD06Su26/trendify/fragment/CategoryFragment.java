3452a435d71a90017a3e01a6c773aedcf74bac85
duyhungdevaaa <machhung1720@gmail.com>
2026-07-04 17:10:29 +0700

    thêm chức năng thêm và sửa sản phẩm

--- app/src/main/java/fpoly/DatnMD06Su26/trendify/fragment/CategoryFragment.java
+++ app/src/main/java/fpoly/DatnMD06Su26/trendify/fragment/CategoryFragment.java
@@ -1,9 +1,12 @@
 package fpoly.DatnMD06Su26.trendify.fragment;
 
 import fpoly.DatnMD06Su26.trendify.R;
-import fpoly.DatnMD06Su26.trendify.adapter.CategoryAdapter;
-import fpoly.DatnMD06Su26.trendify.model.CategoryItem;
-import fpoly.DatnMD06Su26.trendify.helper.FirestoreHelper;
+
+import fpoly.DatnMD06Su26.trendify.activity.*;
+import fpoly.DatnMD06Su26.trendify.fragment.*;
+import fpoly.DatnMD06Su26.trendify.adapter.*;
+import fpoly.DatnMD06Su26.trendify.model.*;
+import fpoly.DatnMD06Su26.trendify.helper.*;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
