9f4777b439a7574867b4ee09b993526b81371592
ngocdoan421 <Lengocdoan36@gmail.com>
2026-07-11 22:16:32 +0700

    tích hợp thanh điều hướng compose vào các màn hình con

---
+++ app/src/main/java/fpoly/DatnMD06Su26/trendify/adapter/ChatAdapter.java
@@ -0,0 +1,73 @@
+package fpoly.DatnMD06Su26.trendify.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import fpoly.DatnMD06Su26.trendify.R;
+import fpoly.DatnMD06Su26.trendify.model.ChatMessage;
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private List<ChatMessage> messageList;
+
+    public ChatAdapter(List<ChatMessage> messageList) {
+        this.messageList = messageList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return messageList.get(position).getType();
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == ChatMessage.TYPE_USER) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_user, parent, false);
+            return new UserMessageViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_chat_bot, parent, false);
+            return new BotMessageViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ChatMessage message = messageList.get(position);
+        if (holder instanceof UserMessageViewHolder) {
+            ((UserMessageViewHolder) holder).tvUserMessage.setText(message.getText());
+        } else if (holder instanceof BotMessageViewHolder) {
+            ((BotMessageViewHolder) holder).tvBotMessage.setText(message.getText());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return messageList.size();
+    }
+
+    static class UserMessageViewHolder extends RecyclerView.ViewHolder {
+        TextView tvUserMessage;
+
+        UserMessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvUserMessage = itemView.findViewById(R.id.tvUserMessage);
+        }
+    }
+
+    static class BotMessageViewHolder extends RecyclerView.ViewHolder {
+        TextView tvBotMessage;
+
+        BotMessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvBotMessage = itemView.findViewById(R.id.tvBotMessage);
+        }
+    }
+}
