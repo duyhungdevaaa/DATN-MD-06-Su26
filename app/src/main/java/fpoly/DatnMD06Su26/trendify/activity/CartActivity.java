2740c1752f2fafc706bfc4731a139e622d32563b
duyhungdevaaa <machhung1720@gmail.com>
2026-07-17 22:49:19 +0700

    fix lỗi tràn viền trên màn hình nhỏ

--- app/src/main/java/fpoly/DatnMD06Su26/trendify/activity/CartActivity.java
+++ app/src/main/java/fpoly/DatnMD06Su26/trendify/activity/CartActivity.java
@@ -1,11 +1,9 @@
 package fpoly.DatnMD06Su26.trendify.activity;
 
 import fpoly.DatnMD06Su26.trendify.R;
-import fpoly.DatnMD06Su26.trendify.activity.*;
-import fpoly.DatnMD06Su26.trendify.fragment.*;
-import fpoly.DatnMD06Su26.trendify.adapter.*;
-import fpoly.DatnMD06Su26.trendify.model.*;
-import fpoly.DatnMD06Su26.trendify.helper.*;
+import fpoly.DatnMD06Su26.trendify.adapter.CartAdapter;
+import fpoly.DatnMD06Su26.trendify.model.CartItem;
+import fpoly.DatnMD06Su26.trendify.helper.CartManager;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -60,7 +58,8 @@ public class CartActivity extends AppCompatActivity {
                 Toast.makeText(this, "Giỏ hàng đang trống", Toast.LENGTH_SHORT).show();
                 return;
             }
-            startActivity(new Intent(this, ShippingAddressActivity.class));
+            // TODO: Chuyển sang ShippingAddressActivity khi màn hình đó được copy sang
+            Toast.makeText(this, "Thanh toán (Tính năng đang phát triển)", Toast.LENGTH_SHORT).show();
         });
 
         loadCart();
